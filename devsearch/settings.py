bda9134b54340eae0f1e2d9cecacefd99f8ed217
sunny01912 <sunnyraj01912@gmail.com>
2021-11-05 19:56:21 +0530

    jhhh

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b#zx1ji=b(793v-_q%_u_kv$+366tv&&s#s5+xu7cwdtz@5ur_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','developersmeet.herokuapp.com']
 
@@ -231,7 +231,7 @@ AWS_S3_FILE_OVERWRITE=False
 
 
 
-if os.getcwd()=='/app':
-    DEBUG=False
+# if os.getcwd()=='/app':
+#     DEBUG=False
 
 
